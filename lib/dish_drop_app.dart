21073f9810726bc0799e14af3bc72fe394b2935d
Julian Rakow <julian.rakow@yahoo.com>
2025-04-29 15:21:09 +0200

    fix: - make bottom nav bar more persistent (still disappears when opening a recommended recipe) - make recommendation card work with asset images as well

--- lib/dish_drop_app.dart
+++ lib/dish_drop_app.dart
@@ -4,9 +4,7 @@ import 'package:dishdrop_app_projekt/ui/screens/shopping_list_screen.dart';
 import 'package:flutter/material.dart';
 
 class DishDropApp extends StatefulWidget {
-  const DishDropApp({
-    super.key,
-  });
+  const DishDropApp({super.key});
 
   @override
   State<DishDropApp> createState() => _DishDropAppState();
@@ -15,34 +13,98 @@ class DishDropApp extends StatefulWidget {
 class _DishDropAppState extends State<DishDropApp> {
   int activeIndex = 0;
 
+  final List<GlobalKey<NavigatorState>> _navigatorKeys = [
+    GlobalKey<NavigatorState>(),
+    GlobalKey<NavigatorState>(),
+    GlobalKey<NavigatorState>(),
+  ];
+
+  void _onTap(int index) {
+    if (index == activeIndex) {
+      // If you tap again on the same tab, maybe pop to first route
+      _navigatorKeys[index].currentState?.popUntil((route) => route.isFirst);
+    } else {
+      setState(() {
+        activeIndex = index;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: [
-        CategoriesScreen(),
-        RecommendationScreen(),
-        ShoppingListScreen(),
-      ][activeIndex],
+      body: IndexedStack(
+        index: activeIndex,
+        children: [
+          _buildTabNavigator(0, CategoriesScreen()),
+          _buildTabNavigator(1, RecommendationScreen()),
+          _buildTabNavigator(2, ShoppingListScreen()),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: activeIndex,
-        onTap: (index) => setState(() {
-          activeIndex = index;
-        }),
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.menu_book_rounded, size: 40),
-            label: "",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.lightbulb_outline, size: 40),
-            label: "",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.local_grocery_store_outlined, size: 40),
-            label: "",
-          ),
+        onTap: _onTap,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.menu_book_rounded, size: 40), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.lightbulb_outline, size: 40), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.local_grocery_store_outlined, size: 40), label: ""),
         ],
       ),
     );
   }
+
+  Widget _buildTabNavigator(int index, Widget child) {
+    return Navigator(
+      key: _navigatorKeys[index],
+      onGenerateRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (_) => child,
+        );
+      },
+    );
+  }
 }
+
+// class DishDropApp extends StatefulWidget {
+//   const DishDropApp({
+//     super.key,
+//   });
+
+//   @override
+//   State<DishDropApp> createState() => _DishDropAppState();
+// }
+
+// class _DishDropAppState extends State<DishDropApp> {
+//   int activeIndex = 0;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: [
+//         CategoriesScreen(),
+//         RecommendationScreen(),
+//         ShoppingListScreen(),
+//       ][activeIndex],
+//       bottomNavigationBar: BottomNavigationBar(
+//         currentIndex: activeIndex,
+//         onTap: (index) => setState(() {
+//           activeIndex = index;
+//         }),
+//         items: const <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.menu_book_rounded, size: 40),
+//             label: "",
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.lightbulb_outline, size: 40),
+//             label: "",
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.local_grocery_store_outlined, size: 40),
+//             label: "",
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
